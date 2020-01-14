b22f13f0c9678f9a9aaf99b334827f634cb8768f
Chauncey Hoover <me@chaunhoov.com>
2020-01-14 16:41:44 -0500

    Closed #2 - Finished input layout, added stage switching
    Added input layout, modularized each "stage"/section/screen of the app to make it easier for development and possible addition/removal of more stages in the future. Also makes making universal CSS changes to each stage easier.
    
    Added animation for switching between each stage

--- wwwroot/js/main.js
+++ wwwroot/js/main.js
@@ -1,7 +1,14 @@
-const input = $('#input');
-const process = $('#process');
-const output = $('#output');
+// Breadcrumbs at top of page
+const input_header = $('#input');
+const process_header = $('#process');
+const output_header = $('#output');
 
+// Each "stage"/section of the app
+const input_stage = $('#input-section');
+const process_stage = $('#process-section');
+const output_stage = $('#output-section');
+
+// Check box in "Files Available" list
 const check_all = $('#check-all');
 
 /**
@@ -9,26 +16,56 @@ const check_all = $('#check-all');
  * @param {int} stage 0 = input, 1 = process, 2 = output
  */
 function setStage(stage) {
-	let elements = [input, process, output];
+	let elements = [input_header, process_header, output_header];
 	elements.forEach(function(element) {
 		element.toggleClass('active', false);
 	});
-	elements[stage - 1].toggleClass('active', true);
-}
 
-let isInternalChange = false;
-
-function mainBoxChecked() {
-	if (!isInternalChange)
-		$('.file-checkbox').prop('checked', check_all.prop('checked'));
-}
-
-function fileChecked(id) {
-	if (!$(`#f${id}`).prop('checked')) {
-		isInternalChange = true; // prevents `mainBoxChecked` from being fired
-		check_all.prop('checked', false);
-		isInternalChange = false;
+	switch(stage) {
+		case 0:
+			input_stage.css('left', '0%');
+			process_stage.css('left', '100%');
+			output_stage.css('left', '200%');
+			input_header.toggleClass('active', true);
+			break;
+		case 1:
+			input_stage.css('left', '-100%');
+			process_stage.css('left', '0%');
+			output_stage.css('left', '100%');
+			process_header.toggleClass('active', true);
+			break;
+		case 2:
+			input_stage.css('left', '-200%');
+			process_stage.css('left', '-100%');
+			output_stage.css('left', '0%');
+			output_header.toggleClass('active', true);
+			break;
 	}
 }
 
+/**
+ * Called when the "check all" / top check box is clicked
+ */
+function mainBoxChecked() {
+	$('.file-checkbox').prop('checked', check_all.prop('checked'));
+}
+
 check_all.change(mainBoxChecked)
+
+/**
+ * Called when an individual file checkbox is checked
+ * @param {int} id The ID of the row being checked
+ */
+function fileChecked(id) {
+	if (!$(`#f${id}`).prop('checked'))
+		check_all.prop('checked', false);
+	else {
+		let check = true;
+		$('.file-checkbox').each(function() {
+			if (!$(this).prop('checked')) 
+				check = false;
+		});
+		check_all.prop('checked', check);
+	}
+}
+
